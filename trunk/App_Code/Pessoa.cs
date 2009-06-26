8db68ce31863f9bc6396f5cceb4d15ff4baa87d5
onizuka.gm <onizuka.gm@b1b76658-2e29-11de-bd8d-4b29057232bf>
2009-06-26 20:21:55 +0000

    arquivos do projeto

---
+++ trunk/App_Code/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Pessoa
+/// </summary>
+public class Pessoa
+{
+    public string nome;
+    public string sobreNome;
+
+	public Pessoa()
+	{
+        nome = String.Empty;
+        sobreNome = String.Empty;
+	}
+}
---
+++ trunk/App_Code/PessoaDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PessoaDAL
+/// </summary>
+public class PessoaDAL
+{
+    public List<Pessoa> listar()
+    {
+        List<Pessoa> objList = new List<Pessoa>();
+        Pessoa objPessoa = null;
+
+        objPessoa = new Pessoa();
+        objPessoa.nome = "Um Nome";
+        objPessoa.sobreNome = "de Teste";
+        objList.Add(objPessoa);
+        objPessoa = null;
+
+
+        objPessoa = new Pessoa();
+        objPessoa.nome = "Nome 2";
+        objPessoa.sobreNome = " Outro Teste";
+        objList.Add(objPessoa);
+        objPessoa = null;
+
+        objList.Sort(delegate(Pessoa p1, Pessoa p2) { return p1.nome.CompareTo(p2.nome); });
+
+        return objList;
+    }
+}
