4667721cf999e437ff645135737525f5e9058308
onizuka.gm <onizuka.gm@b1b76658-2e29-11de-bd8d-4b29057232bf>
2009-06-26 20:21:55 +0000

    arquivos do projeto

---
+++ Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AjaxPro;
+using AjaxPro.Services;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Utility.RegisterTypeForAjax(typeof(_Default));
+    }
+
+    [AjaxMethod]
+    public String listar()
+    {
+        PessoaDAL DALPessoa = new PessoaDAL();
+        List<Pessoa> objPessoa = DALPessoa.listar();
+        String str = JavaScriptSerializer.Serialize(objPessoa);
+
+        return str;
+    }
+}
